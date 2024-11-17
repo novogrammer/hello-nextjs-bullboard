40ec796329e0e1bfdcdccbf7c2ada659c9cec271
Yusuke Kawamoto <novogrammer@gmail.com>
2024-11-18 01:16:49 +0900

    express対応

--- next-app/server/index.ts
+++ next-app/server/index.ts
@@ -1,21 +1,22 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import express, { Request, Response } from "express";
+import next from "next";
 
 const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
-
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+app.prepare().then(()=>{
+  const server = express();
+  server.all("*", (req: Request, res: Response) => {
+    return handle(req, res);
+  });
+  server.listen(port, (err?: any) => {
+    if (err) throw err;
+    console.log(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`
+    );
+  });
+});
